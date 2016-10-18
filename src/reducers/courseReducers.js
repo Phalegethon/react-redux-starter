d1145a98e6d9b9698e888769f095ff86baf0dc4b
S.Gurkan <sgsuerdem@gmail.com>
2016-10-18 18:57:49 +0300

    async writes in redux

--- src/reducers/courseReducers.js
+++ src/reducers/courseReducers.js
@@ -4,6 +4,7 @@ export default function courseReducer(state = [], action) {
   switch (action.type) {
     case types.LOAD_COURSES_SUCCESS:
       return action.courses;
+      
     default:
       return state;
   }
