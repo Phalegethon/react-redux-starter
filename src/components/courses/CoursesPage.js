7255a3e7d287960dcd579486e64f4f93072c5aa1
gsuerdem <sgsuerdem@gmail.com>
2016-10-19 14:00:55 +0300

    Populate from via mapStateToProps

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -3,23 +3,38 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
 import CourseList from './CourseList';
+import {browserHistory} from 'react-router';
 
 class CoursesPage extends React.Component {
   constructor(props, context) {
     super(props, context);
+
+    this.redirectToAddCoursePage = this
+      .redirectToAddCoursePage
+      .bind(this);
   }
 
   courseRow(course, index) {
     return <div key={index}>{course.title}</div>;
   }
 
+  redirectToAddCoursePage() {
+    browserHistory.push('/course');
+  }
+
   render() {
     const {courses} = this.props;
     return (
-        <div>
-          <h1>Courses</h1>
-          <CourseList courses={courses}/>
-        </div>
+      <div>
+        <h1>Courses</h1>
+        <input
+          type="submit"
+          value="Add Course"
+          className="btn btn-primary"
+          onClick={this.redirectToAddCoursePage}/>
+
+        <CourseList courses={courses}/>
+      </div>
     );
   }
 }
@@ -30,13 +45,11 @@ CoursesPage.propTypes = {
 };
 
 function mapStateToProps(state, ownProps) {
-  return {
-    courses: state.courses
-  };
+  return {courses: state.courses};
 }
 
 function mapDispatchToProps(dispatch) {
-  return{
+  return {
     actions: bindActionCreators(courseActions, dispatch)
   };
 }
--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -12,18 +12,55 @@ class ManageCoursePage extends React.Component {
       course: Object.assign({}, this.props.course),
       errors: {}
     };
+
+    this.updateCourseState = this
+      .updateCourseState
+      .bind(this);
+
+    this.saveCourse = this
+      .saveCourse
+      .bind(this);
+  }
+
+  updateCourseState(event) {
+    const field = event.target.name;
+    let course = this.state.course;
+    course[field] = event.target.value;
+    return this.setState({course: course});
+  }
+
+  saveCourse(event) {
+    event.preventDefault();
+    this
+      .props
+      .actions
+      .saveCourse(this.state.course);
+    this
+      .context
+      .router
+      .push('/courses');
   }
 
   render() {
     return (<CourseForm
-      allAuthors={[]}
+      allAuthors={this.props.authors}
+      onChange={this.updateCourseState}
+      onSave={this.saveCourse}
       course={this.state.course}
       errors={this.state.errors}/>);
   }
 }
 
-ManageCoursePage.PropTypes = {
-  course: PropTypes.object.isRequired
+ManageCoursePage.propTypes = {
+  course: PropTypes.object.isRequired,
+  authors: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired
+};
+
+// pull in the React Router context so router is available on
+// this.context.router
+ManageCoursePage.contextTypes = {
+  router: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
@@ -35,7 +72,17 @@ function mapStateToProps(state, ownProps) {
     length: '',
     category: ''
   };
-  return {course: course};
+
+  const authorsFormattedForDropdown = state
+    .authors
+    .map(author => {
+      return {
+        value: author.id,
+        text: author.firstName + ' ' + author.lastName
+      };
+    });
+
+  return {course: course, authors: authorsFormattedForDropdown};
 }
 
 function mapDispatchToProps(dispatch) {
