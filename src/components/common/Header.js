b71cb99921c74f5ce47e7d72aa57a364a832bf31
gsuerdem <sgsuerdem@gmail.com>
2016-10-19 14:41:47 +0300

    added loadingdots component

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import {Link, IndexLink} from 'react-router';
-
+import LoadingDots from './LoadingDots';
 
 const Header = () => {
   return (
-      <nav>
-        <IndexLink to="/" activeClassName="active"> Home</IndexLink>
-        {" | "}
-        <IndexLink to="/about" activeClassName="active"> About</IndexLink>
-        {" | "}
-        <IndexLink to="/courses" activeClassName="active"> Courses</IndexLink>
-      </nav>
+    <nav>
+      <IndexLink to="/" activeClassName="active">
+        Home
+      </IndexLink>
+      {" | "}
+      <Link to="/about" activeClassName="active">
+        About
+      </Link>
+      {" | "}
+      <Link to="/courses" activeClassName="active">
+        Courses
+      </Link>
+      <LoadingDots interval={100} dots={20}/>
+    </nav>
   );
 };
 
